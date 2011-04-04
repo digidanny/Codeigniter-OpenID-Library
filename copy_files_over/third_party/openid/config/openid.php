0c6bdc1c87b4aa07b0662f60c2c009c384e9b6c9
Danny Tran <dannytran@DigiSynd.local>
2011-04-04 13:35:37 -0500

    Left out some files on last commit.

---
+++ copy_files_over/third_party/openid/config/openid.php
@@ -0,0 +1,9 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['openid_storepath'] = 'tmp';
+$config['openid_request_to'] = 'login/finish';
+$config['openid_user_url'] = "";
+
+$config['openid_exclude_controllers'] = array(
+  'login','logout'
+);
